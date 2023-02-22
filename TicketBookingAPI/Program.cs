6986eada96056d0a1e12fbe908e98cbf5f8e0201
hnthinh55 <82455169+hnthinh55@users.noreply.github.com>
2023-02-22 17:01:51 +0700

    Setup logging and handle global error (#18)
    * Setup logging and handle global error
    
    * change location file warning
    
    * Create controller to test logging and exception
    
    * add ILogger into MathController
    
    * fix convention for MathController
    
    * add logging error into ExceptionMiddlewareExtension
    
    * update middleware HandleExceptionMiddleware
    
    * update log and response in HandleExceptionMiddleware

--- TicketBookingAPI/Program.cs
+++ TicketBookingAPI/Program.cs
@@ -1,15 +1,22 @@
+using AutoWrapper;
+using ErrorManagement.Configurations;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using TicketBooking.Data.DbContext;
 using TicketBooking.Data.Infrastructure;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Host.UseSerilog(Log.Logger);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<TicketBookingDbContext>(op =>
     op.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -26,7 +33,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseMiddleware<HandleExceptionMiddleware>();
 
 app.MapControllers();
+app.UseApiResponseAndExceptionWrapper();
 
 app.Run();
