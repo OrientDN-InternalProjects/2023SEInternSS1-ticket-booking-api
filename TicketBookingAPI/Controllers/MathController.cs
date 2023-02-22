154d94e64257ef69861443581b8e1375469f9870
Thinh Huynh <hnthinh55@gmail.com>
2023-02-22 13:06:54 +0700

    fix convention for MathController

--- TicketBookingAPI/Controllers/MathController.cs
+++ TicketBookingAPI/Controllers/MathController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualBasic;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 using TicketBooking.Common.AppExceptions;
 
 namespace TicketBookingAPI.Controllers
@@ -15,19 +12,20 @@ namespace TicketBookingAPI.Controllers
         {
             _logger = logger;
             _logger.LogInformation("Writing to log file with INFORMATION severity level.");
-        
+
             _logger.LogWarning("Writing to log file with WARNING severity level.");
-        
+
             _logger.LogError("Writing to log file with ERROR severity level.");
 
-}
+        }
         [HttpGet]
-        public decimal Divide(decimal Numerator, decimal Denominator)
+        public decimal Divide(decimal numerator, decimal denominator)
         {
             try
             {
-                return (Numerator / Denominator);
-            }catch
+                return (numerator / denominator);
+            }
+            catch
             {
 
                 throw new AppException("Parameter is incorrect");
