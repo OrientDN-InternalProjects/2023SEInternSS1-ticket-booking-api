9e57bf722a61a6cafe720fdbf2a465f713c2c997
Thinh Huynh <hnthinh55@gmail.com>
2023-02-22 10:41:08 +0700

    Setup logging and handle global error

---
+++ TicketBookingAPI/Middlewares/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,55 @@
+﻿using AutoWrapper.Wrappers;
+using Microsoft.AspNetCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TicketBooking.Common.AppExceptions;
+
+namespace TicketBooking.Common.Middlewares
+{
+    public class ExceptionMiddlewareExtension
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddlewareExtension(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (error)
+                {
+                    case AppException e:
+                        // custom application error
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+                    case KeyNotFoundException e:
+                        // not found error
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default:
+                        // unhandled error
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(new { message = error?.Message });
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
--- TicketBookingAPI/Program.cs
+++ TicketBookingAPI/Program.cs
@@ -1,32 +1,55 @@
 using Microsoft.EntityFrameworkCore;
 using TicketBooking.Data.DbContext;
 using TicketBooking.Data.Infrastructure;
+using AutoWrapper;
+using Serilog;
+using TicketBooking.Common.Middlewares;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<TicketBookingDbContext>(op =>
-    op.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.FromLogContext()
+        .CreateLogger();
+    builder.Host.UseSerilog(Log.Logger);
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddDbContext<TicketBookingDbContext>(op =>
+        op.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+    app.UseMiddleware<ExceptionMiddlewareExtension>();
+
+    app.MapControllers();
+    app.UseApiResponseAndExceptionWrapper();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch(Exception ex)
+{
+    Log.Fatal(ex, "Unhandle exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
